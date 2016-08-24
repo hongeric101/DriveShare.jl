699348c16a53bef0503d569a72c6b8986c5304df
Eric Hong <erichong@stanford.edu>
2016-08-24 14:47:19 -0700

    changed to get_dataset(:trace or :highlevel)

--- src/DriveShare.jl
+++ src/DriveShare.jl
@@ -1,5 +1,5 @@
 module DriveShare
-export get_anonymized_paths, get_highlevel_info
+export get_dataset
 
 using Requests: get, save
 using DataFrames
@@ -25,59 +25,14 @@ function getSegmentSummary()
 end
 =#
 
-function get_anonymized_paths()
-	save(get("http://driveshare.me/anonUTM"), "anonUTM.json")
-	JSON.parsefile("anonUTM.json", dicttype=Dict, use_mmap=true)
-end
-
-function get_highlevel_info()
-	save(get("http://driveshare.me/highlevel"), "highlevel.csv")
-	readtable("highlevel.csv")
-end
-
-
-function decodePolyline(str::AbstractString)
-	# returns a list of (lat, lon) Float64 pts
-	# reference: https://github.com/mapbox/polyline/blob/master/src/polyline.js
-
-	retval = Tuple{Float64, Float64}[]
-
-	lat, lon = 0.0, 0.0
-	mult_factor = 1e5
-
-	index = 0
-	while index < length(str)
-
-		byte = convert(UInt8, 0x20)
-		shift = 0
-		result = 0
-
-		while byte ≥ 0x20
-			byte = convert(UInt8, str[index+=1] - 63)
-			result |= convert(Int, byte & 0x1f) << shift
-			shift += 5
-		end
-
-		latitude_change = ((result & 1) > 0 ? ~(result >> 1) : (result >> 1))
-
-		byte = convert(UInt8, 0x20)
-		shift = result = 0;
-
-		while byte ≥ 0x20
-			byte = convert(UInt8, str[index+=1] - 63)
-			result |= convert(Int, byte & 0x1f) << shift
-			shift += 5
-		end
-
-		longitude_change = ((result & 1) > 0 ? ~(result >> 1) : (result >> 1))
-
-		lat += latitude_change
-        lon += longitude_change
-
-        push!(retval, (lat/mult_factor, lon/mult_factor))
+function get_dataset(version)
+	if version == :trace
+		save(get("http://driveshare.me/anonUTM"), "anonUTM.json")
+		return JSON.parsefile("anonUTM.json", dicttype=Dict, use_mmap=true)
+	elseif version == :highlevel
+		save(get("http://driveshare.me/highlevel"), "highlevel.csv")
+		return readtable("highlevel.csv")
 	end
-
-	retval
 end
 
 end
